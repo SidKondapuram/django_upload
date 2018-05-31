2b9e686ad5e87dc54610d1e771b921a76f6a1845
Sid Kondapuram <130640k@acadiau.ca>
2018-05-31 10:25:06 -0300

    Added functionality

--- medinet_convertor/settings.py
+++ medinet_convertor/settings.py
@@ -139,4 +139,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/upload/'
--- medinet_convertor/urls.py
+++ medinet_convertor/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,13 +28,9 @@ urlpatterns += [
     path('upload/', include('upload.urls')),
 ]
 
-from django.views.generic import RedirectView
 urlpatterns += [
-    path('', RedirectView.as_view(url='/upload/')),
+    path('', RedirectView.as_view(url='/accounts/login/')),
 ]
-
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
